32cf0fe94fa4f359243a63004c3e60a33f4c9569
Alex Vizcaino <avizcaino@uxland.es>
2019-09-09 12:55:49 +0200

    update readme

---
+++ test/unit/sandbox.spec.ts
@@ -0,0 +1,27 @@
+import { deserialize } from '../../src/deserialize';
+import { serialize } from '../../src/serialize';
+
+describe('Sandbox', () => {
+  it('Plain to Plain', () => {
+    const input = { foo: 'bar' };
+    const serializers: any = [{ from: 'foo', to: 'FOO' }];
+    expect(serialize(input, serializers)).toStrictEqual({ FOO: 'bar' });
+  });
+  it('Object to Plain', () => {
+    const input = { foo: { baz: 'bar' } };
+    const serializers: any = [{ from: 'foo.baz', to: 'foo' }];
+    expect(serialize(input, serializers)).toStrictEqual({ foo: 'bar' });
+  });
+  it('Plain to Object', () => {
+    const input = { foo: 'bar' };
+    const serializers: any = [{ from: 'foo', to: 'foo.baz' }];
+    expect(serialize(input, serializers)).toStrictEqual({ foo: { baz: 'bar' } });
+  });
+  it('test', () => {
+    const input = { foo: { baz: 'bar' } };
+    const serializers: any = [{ from: 'foo.baz', to: 'baz' }];
+    const output = serialize(input, serializers); // {foo: 'bar'};
+    expect(serialize(input, serializers)).toStrictEqual({ baz: 'bar' });
+    expect(deserialize(output, serializers)).toStrictEqual(input); // {foo: {baz: 'bar'}};
+  });
+});
